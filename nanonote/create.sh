8ba6c6f01be05146876f39156fb579da5d6e5787
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2026-02-04 20:58:42 -0400

    Add nanonote

---
+++ nanonote/create.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+BASEDIR=`dirname -- "$0"` || exit $?
+BASEDIR=`realpath -- "${BASEDIR}"` || exit $?
+
+JAIL="${1:-nanonote}"
+
+appjail makejail \
+	-j "${JAIL}" \
+	-o x11 \
+	-o copydir="${BASEDIR}/files" \
+	-o file="/etc/rc.conf.local" \
+	-o template="${BASEDIR}/template.conf" \
+	-o virtualnet=":<random> default" \
+	-o nat
---
+++ nanonote/run.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+BASEDIR=`dirname -- "$0"` || exit $?
+BASEDIR=`realpath -- "${BASEDIR}"` || exit $?
+
+JAIL="${1:-nanonote}"
+
+XEPHYR_ARGS="\
+-resizeable \
++extension RANDR \
++extension RENDER \
++extension GLX \
++extension XVideo \
++extension DOUBLE-BUFFER \
++extension SECURITY \
++extension DAMAGE \
++extension X-Resource \
+-extension \
+XINERAMA \
++extension MIT-SHM \
+-nolisten tcp"
+
+exec appjail x11 "${JAIL}" \
+	exec_start="ratpoison" \
+	exec_user="nanonote" \
+	xephyr_user="${USER}" \
+	xephyr_args="${XEPHYR_ARGS}"
