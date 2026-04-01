e61b19839d30f12906c3444127db2abefa2af742
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2026-04-01 01:11:55 -0400

    *: Specify absolute path to icon

--- librewolf/install.sh
+++ librewolf/install.sh
@@ -10,5 +10,6 @@ appjail cmd jaildir chown "${UID}:${GID}" "${HOME}/.local/share/applications/${D
 sed -i '' -Ee "s|^Exec=librewolf (.+)|Exec=${APPDIR}/START \1|" "${HOME}/.local/share/applications/${DESKTOPFILE}" || exit $?
 sed -i '' -Ee 's|^Name=(.+)|Name=\1 (AppJail)|' "${HOME}/.local/share/applications/${DESKTOPFILE}" || exit $?
 sed -i '' -Ee 's|^Name\[(.+)\]=(.+)|Name[\1]=\2 (AppJail)|' "${HOME}/.local/share/applications/${DESKTOPFILE}" || exit $?
+sed -i '' -Ee "s|^Icon=.+|Icon=${HOME}/.local/share/pixmaps/librewolf.png|" "${HOME}/.local/share/applications/${DESKTOPFILE}" || exit $?
 appjail cmd local "${X11APPJAIL_JAIL}" cp -a usr/local/lib/librewolf/browser/chrome/icons/default/default48.png "${HOME}/.local/share/pixmaps/librewolf.png" || exit $?
 appjail cmd jaildir chown "${UID}:${GID}" "${HOME}/.local/share/pixmaps/librewolf.png" || exit $?
--- nanonote/install.sh
+++ nanonote/install.sh
@@ -7,5 +7,6 @@ appjail cmd local "${X11APPJAIL_JAIL}" cp -a usr/local/share/applications/com.ag
 appjail cmd jaildir chown "${UID}:${GID}" "${HOME}/.local/share/applications/com.agateau.nanonote.desktop" || exit $?
 sed -i '' -Ee "s|^Exec=.+|Exec=${APPDIR}/START|" "${HOME}/.local/share/applications/com.agateau.nanonote.desktop" || exit $?
 sed -i '' -Ee 's|^Name=(.+)|Name=\1 (AppJail)|' "${HOME}/.local/share/applications/com.agateau.nanonote.desktop" || exit $?
+sed -i '' -Ee "s|^Icon=.+|Icon=${HOME}/.local/share/icons/hicolor/scalable/apps/nanonote.svg|" "${HOME}/.local/share/applications/com.agateau.nanonote.desktop" || exit $?
 appjail cmd local "${X11APPJAIL_JAIL}" cp -a usr/local/share/icons/hicolor/scalable/apps/nanonote.svg "${HOME}/.local/share/icons/hicolor/scalable/apps" || exit $?
 appjail cmd jaildir chown "${UID}:${GID}" "${HOME}/.local/share/icons/hicolor/scalable/apps/nanonote.svg" || exit $?
