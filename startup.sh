a03f571e8103163385c9f7258889862e03cd4f80
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2026-04-06 21:43:25 -0400

    Check if the jail is destroyed for powercut events

--- startup.sh
+++ startup.sh
@@ -32,45 +32,61 @@ if [ -n "${MISSING}" ]; then
     "${EXEC_TOOL}" pkg install -y ${MISSING} || exit $?
 fi
 
-appjail status -q "${X11APPJAIL_JAIL}" > /dev/null 2>&1
+while :; do
+    appjail status -q "${X11APPJAIL_JAIL}" > /dev/null 2>&1
 
-ERRLEVEL=$?
+    ERRLEVEL=$?
 
-if [ ${ERRLEVEL} -gt 1 ]; then
-    if [ -n "${LINUX_VERSION}" ]; then
-        appjail fetch debootstrap "${LINUX_VERSION}" || exit $?
-        export X11APPJAIL_OSVERSION="${LINUX_VERSION}"
-    else
-        if [ `uname -K` -lt 1500000 ]; then
-            if [ -z "${X11APPJAIL_OSVERSION}" ]; then
-                X11APPJAIL_OSVERSION=`freebsd-version | grep -Eo '[0-9]+\.[0-9]+-[a-zA-Z0-9]+'` || exit $?
-                export X11APPJAIL_OSVERSION
-            fi
-
-            appjail fetch www -v "${X11APPJAIL_OSVERSION}" || exit $?
+    if [ ${ERRLEVEL} -gt 1 ]; then
+        if [ -n "${LINUX_VERSION}" ]; then
+            appjail fetch debootstrap "${LINUX_VERSION}" || exit $?
+            export X11APPJAIL_OSVERSION="${LINUX_VERSION}"
         else
-            if [ -z "${X11APPJAIL_OSVERSION}" ]; then
-                freebsd_version=`freebsd-version -k` || exit $?
-                X11APPJAIL_OSVERSION=`printf "%s" "${freebsd_version}" | grep -Eo '^[0-9]+'` || exit $?
-                export X11APPJAIL_OSVERSION
+            if [ `uname -K` -lt 1500000 ]; then
+                if [ -z "${X11APPJAIL_OSVERSION}" ]; then
+                    X11APPJAIL_OSVERSION=`freebsd-version | grep -Eo '[0-9]+\.[0-9]+-[a-zA-Z0-9]+'` || exit $?
+                    export X11APPJAIL_OSVERSION
+                fi
+
+                appjail fetch www -v "${X11APPJAIL_OSVERSION}" || exit $?
+            else
+                if [ -z "${X11APPJAIL_OSVERSION}" ]; then
+                    freebsd_version=`freebsd-version -k` || exit $?
+                    X11APPJAIL_OSVERSION=`printf "%s" "${freebsd_version}" | grep -Eo '^[0-9]+'` || exit $?
+                    export X11APPJAIL_OSVERSION
+                fi
+
+                appjail fetch pkgbase -v "${X11APPJAIL_OSVERSION}" || exit $?
             fi
 
-            appjail fetch pkgbase -v "${X11APPJAIL_OSVERSION}" || exit $?
+            env PAGER=cat appjail update release -v "${X11APPJAIL_OSVERSION}" || exit $?
         fi
 
-        env PAGER=cat appjail update release -v "${X11APPJAIL_OSVERSION}" || exit $?
+        ./create.sh || exit $?
+
+        if [ -n "${X11APPJAIL_ALLOW_HOST}" ]; then
+            X11_DISPLAY=`appjail x11 "${X11APPJAIL_JAIL}" assign_only` || exit $?
+
+            xauth add localhost:${X11_DISPLAY} MIT-MAGIC-COOKIE-1 $(openssl rand -hex 16) || exit $?
+        fi
+
+        break
+    elif [ ${ERRLEVEL} -eq 1 ]; then
+        appjail start -- "${X11APPJAIL_JAIL}" || exit $?
+
+        # If jail is ephemeral, it could be destroyed at this point.
+        appjail status -q "${X11APPJAIL_JAIL}" > /dev/null 2>&1
+
+        if [ $? -gt 1 ]; then
+            # Assuming the jail has been destroyed.
+            continue
+        fi
+
+        break
     fi
 
-    ./create.sh || exit $?
-
-    if [ -n "${X11APPJAIL_ALLOW_HOST}" ]; then
-        X11_DISPLAY=`appjail x11 "${X11APPJAIL_JAIL}" assign_only` || exit $?
-
-        xauth add localhost:${X11_DISPLAY} MIT-MAGIC-COOKIE-1 $(openssl rand -hex 16) || exit $?
-    fi
-elif [ ${ERRLEVEL} -eq 1 ]; then
-    appjail start -- "${X11APPJAIL_JAIL}" || exit $?
-fi
+    break
+done
 
 if [ -z "${X11APPJAIL_INSTALL}" ]; then
     XEPHYR_PID=`appjail jail list -HI -j "${X11APPJAIL_JAIL}" xephyr_pid` || exit $?
