5d54eccd98f2653b8579b56cbd5e59dcd4ae9bda
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2026-04-16 20:02:15 -0400

    brave: Fix .desktop installation path

--- Apps/brave/install.sh
+++ Apps/brave/install.sh
@@ -9,7 +9,7 @@ mkdir -p -- "${ICONDIR}" || exit $?
 DESKTOPFILE="brave-browser.desktop"
 
 rm -f -- "${HOME}/.local/share/applications/${X11APPJAIL_PROFILE}-${DESKTOPFILE}"
-appjail cmd local "${X11APPJAIL_JAIL}" cp -a usr/share/applications/${DESKTOPFILE} "${HOME}/.local/share/applications/${X11APPJAIL_PROFILE}/${X11APPJAIL_PROFILE}-${DESKTOPFILE}" || exit $?
+appjail cmd local "${X11APPJAIL_JAIL}" cp -a usr/share/applications/${DESKTOPFILE} "${HOME}/.local/share/applications/${X11APPJAIL_PROFILE}-${DESKTOPFILE}" || exit $?
 appjail cmd jaildir chown "${X11APPJAIL_UID}:${X11APPJAIL_GID}" "${HOME}/.local/share/applications/${X11APPJAIL_PROFILE}-${DESKTOPFILE}" || exit $?
 sed -i '' -Ee "s|^Exec=/usr/bin/brave-browser-stable|Exec=${X11APPJAIL_APPDIR}/START|" "${HOME}/.local/share/applications/${X11APPJAIL_PROFILE}-${DESKTOPFILE}" || exit $?
 sed -i '' -Ee "s|^Name=Brave Web Browser|Name=Brave Web Browser (AppJail:${X11APPJAIL_PROFILE})|" "${HOME}/.local/share/applications/${X11APPJAIL_PROFILE}-${DESKTOPFILE}" || exit $?
