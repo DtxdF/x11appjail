126654b71a0ac0e8a93cbc1c364a8f042207fab4
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2026-04-18 14:37:24 -0400

    chromium: Wrap chromium with dbus-run-session

--- Apps/chromium/wrapper.sh
+++ Apps/chromium/wrapper.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-exec appjail cmd jexec "${X11APPJAIL_JAIL}" -U noroot -e DISPLAY=":${X11APPJAIL_DISPLAY}" "${X11APPJAIL_APPBIN}" --ignore-gpu-blocklist "$@"
+exec appjail cmd jexec "${X11APPJAIL_JAIL}" -U noroot -e DISPLAY=":${X11APPJAIL_DISPLAY}" \
+    dbus-run-session -- "${X11APPJAIL_APPBIN}" --ignore-gpu-blocklist "$@"
