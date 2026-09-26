879bf63033efc4a559f3eb76fd9de4b0f8326f63
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2026-09-26 01:52:03 -0400

    Add hardware acceleration to ${EXAMPLESDIR}/Apps/thunderbird

---
+++ share/examples/x11appjail/Apps/thunderbird/perms.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+ENABLE_3D_ALREADY=false
+SOUND_ALREADY=false
+
+if x11appjail sys-attr check users.${X11APPJAIL_UID}.perms; then
+    PERMS=`x11appjail sys-attr cat users.${X11APPJAIL_UID}.perms`
+
+    for PERM in ${PERMS}; do
+        if [ "${PERM}" = "enable_3d" ]; then
+            if ${ENABLE_3D_ALREADY}; then
+                continue
+            fi
+
+            PERM_ATTR="${X11APPJAIL_APPNAME}:${X11APPJAIL_PROFILE}.allow.enable_3d"
+
+            if ! x11appjail sys-attr check "${PERM_ATTR}" "${X11APPJAIL_UID}"; then
+                continue
+            fi
+
+            ENABLE_3D_ALREADY=true
+        elif [ "${PERM}" = "sound" ]; then
+            if ${SOUND_ALREADY}; then
+                continue
+            fi
+
+            PERM_ATTR="${X11APPJAIL_APPNAME}:${X11APPJAIL_PROFILE}.allow.sound"
+
+            if ! x11appjail sys-attr check "${PERM_ATTR}" "${X11APPJAIL_UID}"; then
+                continue
+            fi
+
+            SOUND_ALREADY=true
+        fi
+    done
+fi
